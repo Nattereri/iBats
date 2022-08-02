c0eb277fde28777d03be9cd32473d4e8ea5504af
Steve Markham <Nattereri@users.noreply.github.com>
2022-08-02 12:53:46 +0100

    Number of nights function added

---
+++ R/num_of_nights.R
@@ -0,0 +1,30 @@
+#' Number nights between bat observations
+#'
+#' Calculates the number nights between first and last bat observation (plus 1 for dawn)
+#'
+#' @param night Vector of Dates
+#'
+#' @return Integer of the number of nights
+#'
+#' @examples
+#' date_vector <- seq.Date(from=Sys.Date(), length.out = 14L, by='days')
+#' No_Nights(date_vector)
+#'
+#' @export
+No_Nights <- function(night_vector){
+  
+  if(lubridate::is.Date(night_vector)) {
+    
+    as.integer(difftime(max(night_vector, na.rm = T) , 
+                        min(night_vector, na.rm = T) , 
+                        units = c("days"))) + 1
+    
+  } else {
+    
+    print("Night is not in Date format")
+    
+  }
+  
+  
+}
+
