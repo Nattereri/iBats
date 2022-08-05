fe3a1335c345a8f9780a6e064f32418970623b91
Steve Markham <Nattereri@users.noreply.github.com>
2022-08-05 15:03:47 +0100

    night sun times added. SunTimes function made lower case.

---
+++ R/night_sun_times.R
@@ -0,0 +1,45 @@
+#' Merge Suntimes to Bat data frame
+#'
+#' Merges sunset and sunrise times to the bat data frame by Night. Makes a duplicate
+#' of sunset and shifts the the column up one row to to make the sunrise for the Night
+#' (it will do but not yet - fudged at the moment).
+#'
+#' @param bat_df bat data frame with Night column
+#' @sun_df data frame of sun set and sun rise times by Day
+#'
+#' @return bat data frame with un set and sun rise times by Night
+#'
+#' @examples
+#' NightSunTimes
+#'
+#'
+#'
+#'
+#' @export
+night_sun_times <- function(bat_df, sun_df) {
+
+  # Duplicate data.frame to make sunrise data
+  risedata <- sun_df
+
+  # Select  sunset date and time
+  setdata <- sun_df %>%
+    select(date, sunset) %>%
+    mutate(date = lubridate::as_date(date))
+
+  # Select  sunrise date and time
+  risedata <- risedata %>%
+    select(date, sunrise) %>%
+    mutate(date = lubridate::as_date(date),
+           date = date - lubridate::days(1))
+
+
+  # Combine sunset and risedata by
+  sun_data <- dplyr::full_join(setdata, risedata, by="date")
+
+  # Rename date as Night
+  colnames(sun_data) <- c("Night", "sunset", "sunrise")
+
+  # Join bat data with the sundata by Night
+  dplyr::left_join(bat_clean_data, sun_data, by="Night")
+
+}
--- R/sun_times.R
+++ R/sun_times.R
@@ -4,17 +4,17 @@
 #'
 #' @param bat_df bat data frame with Night, Latitude and Longitude columns
 #'
-#' @return night and suntimes data frame
+#' @return night and sun times data frame
 #'
 #' @examples
 #' bat_data_frame <- tibble(Night = Sys.Date(), Latitude = 51.5023, Longitude = -0.1352)
-#' SunTimes(bat_data_frame)
+#' sun_times(bat_data_frame)
 #'
 #' bat_data_frame <- tibble(Night = Sys.Date(), Latitude = -6.1370, Longitude = 106.8497)
-#' SunTimes(bat_data_frame, time_zone = "Asia/Jakarta")
+#' sun_times(bat_data_frame, time_zone = "Asia/Jakarta")
 #'
 #' @export
-SunTimes <- function(bat_df, time_zone = "Europe/London"){
+sun_times <- function(bat_df, time_zone = "Europe/London"){
 
   #check column names
   bat_df_col_names <- colnames(bat_df)
