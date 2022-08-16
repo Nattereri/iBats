dc6cd4e10afcf672505ac8a87891281c13b58265
Steve Markham <Nattereri@users.noreply.github.com>
2022-08-16 11:47:32 +0100

    static data 2 added

---
+++ R/static_data2.R
@@ -0,0 +1,10 @@
+#' Static bat detector data with call metrics from the Tavy valley South Devon
+#'
+#' One nights of static bat detector data, with call metrics, from the Tavy
+#' valley South Devon. The detector was located beneath an oak tree on a field boundary.
+#'
+#' @docType data
+#'
+#' @usage data(static_data2)
+#'
+"static_data2"
