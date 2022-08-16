ae3b6620231d302be55e738d97d561d657da4157
Steve Markham <Nattereri@users.noreply.github.com>
2022-08-16 12:47:28 +0100

    River added

--- R/static_data2.R
+++ R/static_data2.R
@@ -1,6 +1,6 @@
-#' Static bat detector data with call metrics from the Tavy valley South Devon
+#' Static bat detector data with call metrics from the River Tavy valley South Devon
 #'
-#' One nights of static bat detector data, with call metrics, from the Tavy
+#' One nights of static bat detector data, with call metrics, from the River Tavy
 #' valley South Devon. The detector was located beneath an oak tree on a field boundary.
 #'
 #' @docType data
