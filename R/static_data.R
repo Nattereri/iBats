6c32d40efa722cba7349a2c3a46ce3cabe5f624e
Steve Markham <Nattereri@users.noreply.github.com>
2022-08-16 11:47:32 +0100

    static data added

---
+++ R/static_data.R
@@ -0,0 +1,10 @@
+#' Static bat detector data from South Devon
+#'
+#' Static bat detector data from South Devon. Co-ordinates moved slightly so as
+#' not to disclose the location
+#'
+#' @docType data
+#'
+#' @usage data(static_data)
+#'
+"static_data"
