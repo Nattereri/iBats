718ddd2ecd4674c3e29f832a9edd520714e27b2e
Steve Markham <Nattereri@users.noreply.github.com>
2022-08-16 12:45:17 +0100

    changes back to HEAD

--- R/static_data.R
+++ R/static_data.R
@@ -1,12 +1,7 @@
 #' Static bat detector data from South Devon
 #'
-<<<<<<< HEAD
 #' Static bat detector data from a year of surveys in South Devon. Co-ordinates
 #' moved slightly so as not to disclose the location
-=======
-#' Static bat detector data from South Devon. Co-ordinates moved slightly so as
-#' not to disclose the location
->>>>>>> 6c32d40efa722cba7349a2c3a46ce3cabe5f624e
 #'
 #' @docType data
 #'
