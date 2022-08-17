b3f9d4cc95adc8816ca540cfe4b59f91305d80e0
Steve Markham <Nattereri@users.noreply.github.com>
2022-08-17 16:18:41 +0100

    data set statics with no NAs

---
+++ R/statics.R
@@ -0,0 +1,10 @@
+#' Static bat detector data from South Devon
+#'
+#' Static bat detector data from a year of surveys in South Devon. Co-ordinates
+#' moved slightly so as not to disclose the location
+#'
+#' @docType data
+#'
+#' @usage data(statics)
+#'
+"statics"
