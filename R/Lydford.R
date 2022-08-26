c7d89900b109563dbcc098dd39aeb7e2d54a9851
Steve Markham <Nattereri@users.noreply.github.com>
2022-08-26 10:43:27 +0100

    Lydford data added

---
+++ R/Lydford.R
@@ -0,0 +1,17 @@
+#' Lydford Primary School's bat walk observations
+#'
+#' These are Lydford Primary School's bat walk observations from Wednesday
+#' evening 18^th^ September 2019.  The walk, between the School and the River Lyd,
+#' on the western edge of Dartmoor, started at sunset 6:27pm and continued for
+#' just over an hour.  The observations were made with a Batlogger M bat detector
+#' (https://www.batlogger.com/en/products/batlogger_m/) that records the sound
+#' of the bat as it passes together with the time and postion; using a built-in
+#' Global Positioning System (GPS).  These sounds where then analysed with software
+#' designed to help determine which species made the echolocation;
+#' https://www.batlogger.com/en/products/batexplorer/) was the software used.
+#'
+#' @docType data
+#'
+#' @usage data(Lydford)
+#'
+"Lydford"
