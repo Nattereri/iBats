3edcf86d74db2ad857ec3b0ce040af44cd6a2343
Steve Markham <Nattereri@users.noreply.github.com>
2022-12-01 13:13:14 +0000

    Bat Explorer export data added.

---
+++ R/BatExplorer.R
@@ -0,0 +1,11 @@
+#' Data exported from Elekon AG Bat Explorer software with call metrics
+#'
+#' Evening transect bat detector survey, sound analysis undertaken with Elekon AG
+#' Bat Explorer software and the data exported as a csv file with the call metrics.
+#' The transect was in the the River Tavy valley, South Devon.
+#'
+#' @docType data
+#'
+#' @usage data(BatExplorer)
+#'
+"BatExplorer"
