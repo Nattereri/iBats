191f035b64725f1b39d93147b634b8d46f7b9c6b
Steve Markham <Nattereri@users.noreply.github.com>
2022-12-20 14:56:30 +0000

    Default colours for UK scientific names added to bat_colours.

---
+++ R/MaryTavy.R
@@ -0,0 +1,10 @@
+#' Transect bat detector data as exported from Elekon's BatExplorer sound analysis software
+#'
+#' Results from a bat detector transect around Mary Tavy, Devon in June 2019.
+#' data is exported 'asis' from Elekon's BatExplorer sound analysis software
+#'
+#' @docType data
+#'
+#' @usage data(MaryTavy)
+#'
+"MaryTavy"
--- R/bat_colours.R
+++ R/bat_colours.R
@@ -4,6 +4,7 @@
 #'
 #' @param bat_column Vector of bat species to be graphed and assigned a species colour
 #' @param colour_vector A named vector of 'all' bat species and an assigned colour
+#' @param default_colours_sci TRUE for default colours UK scientific names only
 #'
 #' @return A named vector of bat species and an assigned colour
 #'
@@ -12,18 +13,62 @@
 #' bat_column_to_graph <- c("Myotis", "Common pipistrelle", "Common pipistrelle", "Barbastelle", "Natterer's")
 #' bat_colours(bat_column_to_graph, colour_vector = bat_colours)
 #'
+#' bat_column_to_graph <- c("Barbastella barbastellus", "Myotis daubentonii", "Pipistrellus pygmaeus", "Pipistrellus pygmaeus")
+#' bat_colours(bat_column_to_graph, colour_vector = NULL, default_colours_sci = TRUE)
+#'
 #' scales::show_col(bat_colours(bat_column_to_graph, colour_vector = bat_colours))
 #'
 #' @export
-bat_colours <- function(bat_column_to_graph, colour_vector = bat_colours){
+bat_colours <- function(bat_column_to_graph, colour_vector = bat_colours, default_colours_sci = FALSE){
 
   stopifnot("Input must be character vector" =  is.character(bat_column_to_graph))
-  stopifnot("Input must be character vector" =  is.character(colour_vector))
 
-  graph_bat_list <- levels(factor(bat_column_to_graph))
 
-  #Subset named vector
-  colour_vector[(names(colour_vector) %in% graph_bat_list)]
+  if(default_colours_sci) {
+
+    colour_vector <- c(
+      "Barbastella barbastellus" = "#1f78b4",
+      "Myotis alcathoe" = "#a52a2a",
+      "Myotis bechsteinii" = "#7fff00",
+      "Myotis brandtii" = "#b2df8a",
+      "Myotis mystacinus" = "#6a3d9a",
+      "Myotis nattereri" = "#ff7f00",
+      "Myotis daubentonii" = "#a6cee3",
+      "Myotis spp." = "#bcee68",
+      "Plecotus auritus" = "#8b0000",
+      "Plecotus spp." = "#8b0000",
+      "Plecotus austriacus" = "#000000",
+      "Pipistrellus pipistrellus" = "#ffff99",
+      "Pipistrellus nathusii" = "#8a2be2",
+      "Pipistrellus pygmaeus" = "#b15928",
+      "Pipistrellus spp." = "#fdbf6f",
+      "Rhinolophus ferrumequinum" = "#e31a1c",
+      "Rhinolophus hipposideros" = "#33a02c",
+      "Nyctalus noctula" = "#cab2d6",
+      "Nyctalus leisleri" = "#fb9a99",
+      "Nyctalus spp." = "#eee8cd",
+      "Eptesicus serotinus" = "#008b8b"
+    )
+
+    #colour_vector = bat_colours
+
+    graph_bat_list <- levels(factor(bat_column_to_graph))
+
+    #Subset named vector
+    colour_vector[(names(colour_vector) %in% graph_bat_list)]
+
+  } else {
+
+    stopifnot("Input must be character vector" =  is.character(colour_vector))
+
+    graph_bat_list <- levels(factor(bat_column_to_graph))
+
+    #Subset named vector
+    colour_vector[(names(colour_vector) %in% graph_bat_list)]
+
+  }
+
+
 
 
 }
