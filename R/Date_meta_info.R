4419f49c7088f166ba85db9087e5119f1470d286
Steve Markham <Nattereri@users.noreply.github.com>
2023-12-19 13:54:45 +0000

    Revised date time info.

--- R/Date_meta_info.R
+++ R/Date_meta_info.R
@@ -1,6 +1,6 @@
 #' Add date and time information
 #'
-#' Creates useful date and time information (Night, Month, MonthFull, Year, JustTime)
+#' Creates useful date and time information (Night, Month, etc..)
 #' from the DateTime column, as extra columns in the data frame, from the date
 #' and time of the bat observation. The default IANA time zone is "Europe/London".
 #'
@@ -28,7 +28,12 @@ date_time_info <- function(bat_df, time_zone = "Europe/London") {
   stopifnot("Input must have DateTime column" =  lubridate::is.POSIXct(bat_df$DateTime))
 
 
-  #if("DateTime" %in% bat_df_col_names) {
+  Season_levels <- c("Winter", "Spring", "Summer", "Autumn")
+
+  SummerVector <- c("Jun", "Jul", "Aug" )
+  AutumnVector <- c("Sep", "Oct", "Nov" )
+  WinterVector <- c("Dec", "Jan", "Feb")
+  SpringVector <- c("Mar", "Apr", "May" )
 
     bat_df %>%
       dplyr::mutate(DateTime = lubridate::force_tz(DateTime, time_zone),
@@ -37,9 +42,17 @@ date_time_info <- function(bat_df, time_zone = "Europe/London") {
              Month = lubridate::month(Night, label = T), # Make Month of observation
              MonthFull = lubridate::month(Night, label = T, abbr = F), # Make FullMonth
              Year = lubridate::year(Night), # Make Year of observation
+             Year2 = stringr::str_sub(as.character(Year), 3L, 4L),
+             monYr2 = glue("{Month} {Year2}"),
              JustTime = DateTime + lubridate::hours(12),
              JustTime = hms::as_hms(stringr::str_sub(as.character(JustTime),
-                                                     start = 12, end = 19)))
+                                                     start = 12, end = 19)),
+             Season = case_when(
+               Month %in% WinterVector ~ "Winter",
+               Month %in% SpringVector ~ "Spring",
+               Month %in% SummerVector ~ "Summer",
+               Month %in% AutumnVector ~ "Autumn"),
+             Season = factor(Season, levels = Season_levels))
 
 
 
