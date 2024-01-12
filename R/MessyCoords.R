96f19d5529859b39cc146d12a0ff380b761008b4
Steve Markham <Nattereri@users.noreply.github.com>
2024-01-12 10:43:36 +0000

    Messy coordinates.

---
+++ R/MessyCoords.R
@@ -0,0 +1,10 @@
+#' A data.frame showing different ways to specify longitude and latitude coordinates.
+#'
+#' A data.frame with the longitude and latitude coordinates in different formats
+#' e.g. decimal degrees and degrees minutes seconds
+#'
+#' @docType data
+#'
+#' @usage data(MessyCoords)
+#'
+"MessyCoords"
