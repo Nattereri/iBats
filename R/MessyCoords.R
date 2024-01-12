e67f5dcf3acca534fe26ad335cefbbcdb7868894
Steve Markham <Nattereri@users.noreply.github.com>
2024-01-12 10:59:30 +0000

    Update packages.

--- R/MessyCoords.R
+++ R/MessyCoords.R
@@ -1,4 +1,4 @@
-#' A data.frame showing different ways to specify longitude and latitude coordinates.
+#' A data.frame that specifies longitude and latitude coordinates in different formats.
 #'
 #' A data.frame with the longitude and latitude coordinates in different formats
 #' e.g. decimal degrees and degrees minutes seconds
