5daf12c2787df909f005ee00258696fa1b725bd9
Steve Markham <Nattereri@users.noreply.github.com>
2024-01-17 10:52:33 +0000

    Messy species data.

---
+++ R/MessySpecies.R
@@ -0,0 +1,10 @@
+#' A data.frame that has realistic species ID from sound analysis software.
+#'
+#' It is not uncommon for species ID from sound analysis software to have multiple
+#' species in an individual sound file.
+#'
+#' @docType data
+#'
+#' @usage data(MessySpecies)
+#'
+"MessySpecies"
