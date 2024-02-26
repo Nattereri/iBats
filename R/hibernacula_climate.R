0a917c81aa7114df54dd4ae80108699e817bbcab
Steve Markham <Nattereri@users.noreply.github.com>
2024-02-26 14:22:36 +0000

    Hibernacula climate.

---
+++ R/hibernacula_climate.R
@@ -0,0 +1,10 @@
+#' A time series data.frame listing temperature, humidity and dew point for potential bat hibernacula.
+#'
+#' A time series of temperature, humidity and dew point recorded every hour throughout the winter
+#' in potential bat hibernacula located in southern Britain.
+#'
+#' @docType data
+#'
+#' @usage data(hibernacula_climate)
+#'
+"hibernacula_climate"
