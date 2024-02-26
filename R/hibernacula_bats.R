f6d9506831588c755e35525a6b5cb7e4a1f0e740
Steve Markham <Nattereri@users.noreply.github.com>
2024-02-26 16:22:43 +0000

    Typo temperature.

--- R/hibernacula_bats.R
+++ R/hibernacula_bats.R
@@ -4,7 +4,7 @@
 #' details the date and time bat activity was recorded, the species of bat and the length of
 #' time bats were active in the hibernacula. The hibernacula is located in southern Britain.
 #'
-#' This data set relates to the hibrenacula_climate data set that records temerature and humidity
+#' This data set relates to the hibrenacula_climate data set that records temperature and humidity
 #' for all the buildings observed as a potential hibernacula.
 #'
 #' @docType data
