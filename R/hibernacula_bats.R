9f15d19b9cf64935632d04b45c197d715b39c7ee
Steve Markham <Nattereri@users.noreply.github.com>
2024-02-26 16:16:51 +0000

    Hibernacula bats.

---
+++ R/hibernacula_bats.R
@@ -0,0 +1,14 @@
+#' A data.frame of bat activity in a hibernacula.
+#'
+#' Bat activity in a hibernacula was recorded over a winter period.  This data.frame
+#' details the date and time bat activity was recorded, the species of bat and the length of
+#' time bats were active in the hibernacula. The hibernacula is located in southern Britain.
+#'
+#' This data set relates to the hibrenacula_climate data set that records temerature and humidity
+#' for all the buildings observed as a potential hibernacula.
+#'
+#' @docType data
+#'
+#' @usage data(hibernacula_bats)
+#'
+"hibernacula_bats"
